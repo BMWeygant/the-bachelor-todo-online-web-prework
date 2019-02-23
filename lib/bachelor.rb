49c2d930f5e40b5ef73db9759f19f44491fb5ae3
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-23 04:46:26 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -15,6 +15,10 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season, contestant_hash|
+    #binding.pry
+
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
